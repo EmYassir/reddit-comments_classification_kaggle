291702eb9226b77f86291cec8beb4377e1847375
Alexander Bustamante <alexpehpeh@pop-os.localdomain>
2019-10-13 14:25:09 -0400

    voc spécialisé

--- text_processing_util.py
+++ text_processing_util.py
@@ -9,7 +9,7 @@ from nltk.stem import WordNetLemmatizer
 
 class TextProcessingUtil:
 
-    def __init__(self, frequent_words_amount=2000):
+    def __init__(self, frequent_words_amount=2000, frequent_words_per_label=75):
         nltk.download('stopwords')
         nltk.download('punkt')
         nltk.download('wordnet')
@@ -17,6 +17,7 @@ class TextProcessingUtil:
         self._stop_words = set(stopwords.words('english'))
         self._tokenizer = RegexpTokenizer(r'\w+')
         self._frequent_words_amount = frequent_words_amount
+        self._frequent_words_per_label = frequent_words_per_label
         self._vocabulary = {}
         self._sorted_vocabulary = None
         self._freq_words = None
@@ -28,15 +29,22 @@ class TextProcessingUtil:
     def _remove_stop_words(self, tokenized_sentence):
         return [w for w in tokenized_sentence if w not in self._stop_words]
 
-    def _apply_lemmatizer(self, tokenized_sentence):
+    def _apply_verb_lemmatizer(self, tokenized_sentence):
         lemmas = []
         for token in tokenized_sentence:
-            lemma = self._lemmatizer.lemmatize(token, pos="n")
-            lemmas.append(self._lemmatizer.lemmatize(token, pos="v") if lemma == token else lemma)
+            lemma = self._lemmatizer.lemmatize(token, pos="v")
+            lemmas.append(lemma)
             if lemma not in self._vocabulary.keys():
                 self._vocabulary[lemma] = 1
             else:
-                self._vocabulary[lemma]  += 1
+                self._vocabulary[lemma] += 1
+        return lemmas
+
+    def _apply_noun_lemmatizer(self, tokenized_sentence):
+        lemmas = []
+        for token in tokenized_sentence:
+            lemma = self._lemmatizer.lemmatize(token, pos="n")
+            lemmas.append(lemma)
         return lemmas
 
     def _preprocess_sentence(self, sentence):
@@ -44,12 +52,13 @@ class TextProcessingUtil:
         step2 = self._clean_urls(step1)
         step3 = self._tokenizer.tokenize(step2)
         step4 = self._remove_stop_words(step3)
-        step5 = self._apply_lemmatizer(step4)
-        return step5
+        step5 = self._apply_noun_lemmatizer(step4)
+        step6 = self._apply_verb_lemmatizer(step5)
+        return step6
 
     def _get_bow_representation(self, tokenized_sentence):
         if not self._sorted_vocabulary:
-            self._sorted_vocabulary = sorted(self._freq_words)
+            self._sorted_vocabulary = sorted(set(self._freq_words))
         bow = np.zeros(len(self._sorted_vocabulary))
         for token in tokenized_sentence:
             try:
@@ -63,14 +72,36 @@ class TextProcessingUtil:
         self._labels = np.unique(label_values)
         return list(map((lambda x: np.where(self._labels == x)[0][0]), label_values))
 
+    def _get_specialized_vocabulary(self, tokenized_sentences, numeric_label_values):
+        specialized_vocabulary = {}
+        for index, tokenized_sentence in enumerate(tokenized_sentences):
+            if numeric_label_values[index] not in specialized_vocabulary.keys():
+                specialized_vocabulary[numeric_label_values[index]] = {}
+            for token in tokenized_sentence:
+                if token not in self._freq_words:
+                    if token not in specialized_vocabulary[numeric_label_values[index]].keys():
+                        specialized_vocabulary[numeric_label_values[index]][token] = 1
+                    else:
+                        specialized_vocabulary[numeric_label_values[index]][token] += 1
+
+        for label in specialized_vocabulary.keys():
+            class_dic = specialized_vocabulary[label]
+            class_freqs = heapq.nlargest(self._frequent_words_per_label, class_dic, key=class_dic.get)
+            self._freq_words += class_freqs
+
     def get_vocabulary(self):
         return np.array(self._sorted_vocabulary)
 
     def get_labels(self):
-        return np.copy(self._labels)
+        return self._labels
 
     def get_bow_matrix(self, sentences, label_values=None):
         tokenized_sentences = list(map(self._preprocess_sentence, sentences))
         self._freq_words = heapq.nlargest(self._frequent_words_amount, self._vocabulary, key=self._vocabulary.get)
+        if label_values is not None:
+            numeric_labels = self._get_numeric_labels(label_values)
+            self._get_specialized_vocabulary(tokenized_sentences, numeric_labels)
         bow_reps = list(map(self._get_bow_representation, tokenized_sentences))
-        return np.column_stack((bow_reps, self._get_numeric_labels(label_values))) if label_values else np.array(bow_reps)
+        print(self._sorted_vocabulary)
+        print(len(self._sorted_vocabulary))
+        return np.column_stack((bow_reps, numeric_labels)) if label_values else np.array(bow_reps)
