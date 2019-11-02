6d94e891466f2e97eb42c0c687ac3ec19972ed3b
EmYassir <yassir.elmesbahi@outlook.com>
2019-11-01 22:14:56 -0400

    Dictionary

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -9,25 +9,26 @@ import numpy as np
 
 class Dictionary:
     
-    def __init__(self, n_classes, masked = False):
+    def __init__(self, labels):
+        
         # labels
-        self._labels = np.arrange(n_classes)
+        self._labels = np.array(labels)
+        self._labels_Ids = np.arange(len(labels))
 
         # all words seen so far
+        self._global_count = 0
         self._all_words_counts = {}
         
         # words counts per class 
-        self._words_counts_per_class = np.array([dict() for i in range(n)])
+        self._words_counts_per_class = np.array([dict() for i in range(len(labels))])
         
         # all classes where the word appears
         self._word_classes = {}
-        
-        # all classes where the word appears
-        self._word_classes = {}
+
         
         
     # Gets sorted classes
-    def get_classes(self):
+    def get_labels(self):
         return np.copy(self._labels)
     
     # Update the dictionary
@@ -38,6 +39,8 @@ class Dictionary:
                 self._all_words_counts[word] += 1
             else:
                 self._all_words_counts[word] = 1
+            # Update the global count 
+            self._global_count += 1
             
             # Updating the classes where the word belongs
             dic = self._words_counts_per_class[label]
@@ -78,24 +81,83 @@ class Dictionary:
     
     # Get total number of words per class
     def get_total_count_per_class(self, label):
-        if (label < 0) or (label > self._n_classes):
+        if (label < 0) or (label >= len(self._labels)):
             print('Invalid label id given: could not find label id %d' %label)
             return 0
         return len(self._words_counts_per_class[label])
 
     # Total number of words
     def get_global_count(self):
+        return self._global_count
+
+    # Total number of different words words
+    def get_count_unique_words(self):
         return len(self._all_words_counts)
     
-    # Prints out dictionary
-    def dump_dictionary(self, filepath = './out.txt'):
+    # Returns list of n most popular words per class
+    def get_top_words_per_label(self, n, label = -1):
+        if n <= 0:
+            print('n should be positive')
+            return
+        dic = None
+        # label == -1 means we re returning top words over all corpus
+        if label  == -1:
+            dic = self._all_words_counts
+        else :
+            dic = self._words_counts_per_class[label]
+        
+        sorted_list = sorted(dic, key=dic.get, reverse=True)
+        return sorted_list[:min(len(sorted_list),n)]
+    
+    # Returns list of n least popular words per class
+    def get_bottom_words_per_label(self, n, label = -1):
+        dic = None
+        # label == -1 means we re returning top words over all corpus
+        if label  == -1:
+            dic = self._all_words_counts
+        else :
+            dic = self._words_counts_per_class[label]
+        
+        sorted_list = sorted(dic, key=dic.get, reverse=False)
+        return sorted_list[:min(len(sorted_list), n)]
+    
+        
+    # Returns list of n common words between list of classes passed as parameter
+    def get_words_common_to_labels(self, n, labels = []):
+        res = []
+        if labels  == []:
+            print('Empty list of labels!')
+            return res
+        
+        # Quadratic cost
+        dic = self._all_words_counts
+        count = 0
+        sorted_global_list = sorted(self._all_words_counts, key=dic.get, reverse=True)
+        for word in sorted_global_list:
+            is_word_common = True
+            for l in labels:
+                if word not in self._words_counts_per_class[l]:
+                    is_word_common = False
+                    break
+            # Adding word to the list
+            if (is_word_common == True):
+                res.append(word)
+                count += 1
+            # Checking if max is reached
+            if count == n:
+                break
+        # End
+        return res
+    
+       # Prints out dictionary
+    def dump_dictionary(self, filepath = './dump/total_words.txt'):
         dic = self._all_words_counts
         with open(filepath, 'w',  encoding="utf-8") as file:
             for w in sorted(dic, key=dic.get, reverse=True):
                 file.write('%s : %d\n' %(w, dic[w]))
     
     # Prints out dictionary of classes
-    def dump_dictionary_per_classes(self, max_class, filepath = './out.txt'):
+    def dump_dictionary_per_classes(self, max_class, filepath = './dump/words_per_classes.txt'):
         if max_class < 0 or max_class > 20:
             max_class = 20
         with open(filepath, 'w',  encoding="utf-8") as file:
@@ -106,8 +168,65 @@ class Dictionary:
                     file.write('%s : %d\n' %(w, dic[w]))
                 file.write('\n')
     
-    # Returns list of most popular words per class
-    def get_n_top_words_per_label(number label, number):
+        # Returns list of n most popular words per class
+    #def get_top_words_per_label(self, n, label = -1):
+    def dump_top_words_per_label(self, n, label = -1):
+        filepath = ''
+        if label == -1:
+            filepath = 'all'
+        elif label >-1 :
+            filepath = self._labels[label]
+        else:
+            print('Cannot find label %d' %label)
+        
+        filepath = './dump/top_words_per_label_'+ str(filepath) + '.txt'
+        list_words =  self.get_top_words_per_label(n, label)
+        with open(filepath, 'w',  encoding="utf-8") as file:
+            file.write('Label %s (%d top words):\n' % (self._labels[label], n))
+            for w in list_words:
+                file.write('%s, ' %w)
+            file.write('\n')
+        
+        
+    #def get_bottom_words_per_label(self, n, label = -1):
+    def dump_bottom_words_per_label(self, n, label = -1):
+        filepath = ''
+        if label == -1:
+            filepath = 'all'
+        elif label >-1 :
+            filepath = self._labels[label]
+        else:
+            print('Cannot find label %d' %label)
+        
+        filepath = './dump/bottom_words_per_label_'+ str(filepath) + '.txt'
+        list_words =  self.get_bottom_words_per_label(n, label)
+        with open(filepath, 'w',  encoding="utf-8") as file:
+            file.write('Label %s (%d bottom words):\n' % (self._labels[label], n))
+            for w in list_words:
+                file.write('%s\n' %w)
+            file.write('\n')
+            
+    #def get_words_common_to_labels(self, n, labels = []):
+    def dump_words_common_to_labels(self, n, labels = []):
+        filepath = ''
+        if labels  == []:
+            print('Empty list of labels!')
+            return 
+        else:
+            for l in labels:
+               filepath = filepath + '_' + str(self._labels[l])
+        
+        filepath = './dump/common_words_btw_labels'+ str(filepath) + '.txt'
+        list_words =  self.get_words_common_to_labels(n, labels)
+        with open(filepath, 'w',  encoding="utf-8") as file:
+            file.write('Common words sharing labels: ')
+            for l in labels:
+                file.write('%s ' %self._labels[l])
+            file.write('\n Total of %d words \n' %len(list_words))
+            for w in list_words:
+                file.write('%s, ' %w)
+            file.write('\n')
+        
         
         
     
--- text/text_util.py
+++ text/text_util.py
@@ -18,16 +18,15 @@ nltk.download('wordnet')
 nltk.download('punkt')
 
 
-REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
-BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
-STOPWORDS = set(stopwords.words('english'))
-
 class Text_Util:
+    
     # Init
     def __init__(self):
         self._lemmatizer = WordNetLemmatizer()
         self._stemmer = SnowballStemmer("english", ignore_stopwords=True)
         self._stop_words = set(stopwords.words('english'))
+        self._bad_syms = re.compile('[^0-9a-z #+_]')
+        self._replace_by_space = re.compile('[/(){}\[\]\|@,;#$&]')
         self._tokenizer = RegexpTokenizer(r'[\d.,]+|[A-Z][.A-Z]+\b\.*|\w+|\S')
         self._urls = {'www': 1, 'http': 2, 'https': 3, 'com':4}
     
@@ -52,7 +51,7 @@ class Text_Util:
         return alphanums
     
     
-    def remove_url_remains(self, tokenized_comment):
+    def _remove_url_extra(self, tokenized_comment):
         words = []
         for word in tokenized_comment:
             if word not in self._urls:
@@ -60,41 +59,42 @@ class Text_Util:
         return words
 
     
-    def remove_small_words(self, tokenized_comment):
+    def _remove_small_words(self, tokenized_comment):
         words = []
         for token in tokenized_comment:
             if 1 < len(token):
                 words.append(token)
         return words
     
-    def lemmatize(self, tokenized_comment):
+    def _lemmatize(self, tokenized_comment):
         lemmas = []
         for token in tokenized_comment:
             lemma = self._lemmatizer.lemmatize(token, 'n')
             lemmas.append(lemma)
         return lemmas
     
-    def stem(self, tokenized_comment):
+    def _stem(self, tokenized_comment):
         stems = []
         for token in tokenized_comment:
             stem = self._stemmer.stem(token)
             stems.append(stem)
         return stems
     
-    def get_preprocessed_data(self, data):
+    
+    def get_preprocessed_data_1(self, data):
         # 'data' is an array of comments
         n = data.shape[0]
         results = []
         for i in range(n):
             comment = data[i]   
             comment = comment.replace('_', ' ')
-            aux = self.tokenizer.tokenize(comment.lower())           
-            aux = self.remove_stop_words(aux)   # step1
-            aux = self.remove_url_remains(aux)  # step2
-            aux = self.remove_non_alpha(aux)    # step3
-            aux = self.lemmatize(aux)           # step4
-            aux = self.remove_small_words(aux)  # step5
-            aux = self.stem(aux)                # step6          
+            aux = self._tokenizer.tokenize(comment.lower())           
+            aux = self._remove_stop_words(aux)   # step1
+            aux = self._remove_url_extra(aux)    # step2
+            aux = self._remove_non_alpha(aux)    # step3
+            aux = self._lemmatize(aux)           # step4
+            aux = self._remove_small_words(aux)  # step5
+            aux = self._stem(aux)                # step6          
             results.append(aux)
         return np.array(results)
         
